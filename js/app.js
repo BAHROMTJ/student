01238ce6855eae05d843d5a51370be9306ee80ee
BAHROMTJ <sarifzodabahromofficall@gmail.com>
2023-06-09 10:13:56 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+import {render} from "./dom.js";
+
+let url="http://localhost:3000/student"
+
+let form=document.querySelector('form');
+let arr=[];
+
+form.onsubmit = (elem)=>{
+    elem.preventDefault();
+let target=elem.target;
+let student={
+id:  Math.floor(Math.random() * 100),
+name:target.name.value,
+clas:target.clas.value,
+hist:target.hist.value,
+art:target.art.value,
+points:0
+  
+}
+student.points=Number(student.hist)+Number(student.art)
+
+addStudent(student)
+
+}
+
+
+async function getStudents(){
+
+
+    try {
+        let {data} = await axios.get(url)
+        arr=[...data]
+        render()
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getStudents()
+
+
+
+async function addStudent(student) {
+    try {
+        let{data} = await axios.post(url,student);
+        console.log(data);
+    } catch (error) {
+        
+    }
+}
+
+
+async function daletStudent(id) {
+    try {
+        let{data} = await axios.dalet(`${url}/${id}`);
+    getStudents()
+        console.log(data);
+    } catch (error) {
+        
+    }
+}
+
+
+
+export {
+    arr,
+    daletStudent,
+
+}
---
+++ js/dom.js
@@ -0,0 +1,76 @@
+import {arr,daletStudent} from "./app.js";
+
+
+
+function render() {
+let tbody = document.querySelector("tbody");    
+tbody.innerHTML ="";
+
+arr.forEach(elem => {
+    
+let tr=document.createElement("tr");
+let tdid=document.createElement("td");
+let tdname=document.createElement("td");
+let tdclss=document.createElement("td");
+let tdhistory=document.createElement("td");
+let tdart=document.createElement("td");
+let tdpoints=document.createElement("td");
+let tdeddel=document.createElement("td");
+let btndel=document.createElement("button");
+let btnedit=document.createElement("button");
+btndel.innerHTML="Delete";
+
+btnedit.innerHTML="Edit";
+
+tdid.innerHTML=elem.id;
+
+tdname.innerHTML=elem.name;
+
+tdclss.innerHTML=elem.clas;
+
+tdhistory.innerHTML=elem.hist;
+
+tdart.innerHTML=elem.art;
+
+tdpoints.innerHTML=elem.points;
+
+
+
+
+
+
+tbody.appendChild(tr);
+tr.appendChild(tdid);
+
+tr.appendChild(tdname);
+
+tr.appendChild(tdclss);
+
+tr.appendChild(tdhistory);
+
+tr.appendChild(tdart);
+
+tr.appendChild(tdpoints);
+
+tr.appendChild(tdeddel);
+tdeddel.appendChild(btndel);
+
+tdeddel.appendChild(btnedit);
+
+
+btndel.onclick = ()=>{
+console.log(elem.id);
+    daletStudent(elem.id)
+}
+
+})
+
+
+
+}
+
+
+export{
+    render,
+
+}
